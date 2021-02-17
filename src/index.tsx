e9fb85cbaac2c06fe332592e9fb6c17d7542407f
Manu <manu.villar.vitoria@gmail.com>
2021-02-17 12:55:08 +0100

    setting up the App with Typescript and adding styled components using themes

---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import reportWebVitals from './core/reportWebVitals';
+import { unregister } from './core';
+import { GlobalStyles, theme } from './styles'
+import { ThemeProvider } from 'styled-components';
+import { Content, Title } from './components';
+
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <Content data-cy="content">
+      <Title data-cy="title">Sudoku</Title>
+    </Content>
+  </ThemeProvider>,
+  document.getElementById('root')
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+unregister();
